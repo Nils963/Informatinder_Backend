660ff200bacf784f0192c81ac180c00b788f9a46
Erik Fabian <erik.fabian@stud.hs-flensburg.de>
2022-06-02 13:39:37 +0200

    Potential request aborted fix

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser"
 
 import { checkAuth } from "./middleware/auth.js";
 import routes from "./routes/index.js"
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 app.use(checkAuth);
 
 app.use('/', routes);
