3c6e7b3c1a35e633ab7ae54115db921b7850b566
Erik Fabian <erik.fabian@stud.hs-flensburg.de>
2022-06-13 14:16:20 +0200

    Test CD

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,19 +1,22 @@
 import * as models from "../db.js";
 
 export const getProfile = async (req, res) => {
+  if (req.params.id === undefined) {
+    return res.status(400).json({ error: "No Id given." })
+  }
   const id = Number(req.params.id);
 
   models.Profile.findByPk(id)
     .then(profile => {
       if (!profile) {
-        res.status(404).send({ error: "No Profile with given id." })
+        res.status(404).json({ error: "No Profile with given id." })
       } else {
         res.status(200).json({ profile });
       }
     })
     .catch(err => {
       console.log(err);
-      res.status(500).send("500 - Server error");
+      res.status(500).json({ error: "500 - Server error" });
     });
 }
 
